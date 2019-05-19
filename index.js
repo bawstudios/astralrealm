929281f7b5737cdc6ee1bf4e4d21f0e271e0544f
bawstudios <bawstudios-game@users.noreply.github.com>
2019-05-19 07:04:04 -0700

    One more

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+
+app.get('/', (req, res) => {
+    res.send("Test");
+});
+
+http.listen(process.env.PORT);
